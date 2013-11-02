c384443fcc509f5f819d0a30fc620c4cb428f343
JLiu <xiaoyu.justin.liu@gmail.com>
2013-11-01 19:26:39 -0700

    update

--- Recognition/Segmentation.cpp
+++ Recognition/Segmentation.cpp
@@ -11,7 +11,6 @@ using std::endl;
 //#include "Backbone/Algs.hpp"
 
 
-
 void Segmentation :: execute(imgdata_t &data){
 
         Mat binary;
@@ -29,34 +28,26 @@ void Segmentation :: execute(imgdata_t &data){
                  " before passing to segmentation\n" <<endl;
           return;
         }
-        cout <<"passed 0\n"<<endl; 
       
         image = data.image_data; 
 
-        if(image.empty()){cout <<"image is null"<<endl; return} 
+        if(image.empty()){cout <<"image is null"<<endl; return;} 
 
         cvtColor(image, binary, CV_BGR2GRAY);
         threshold(binary, binary, 100, 255, THRESH_BINARY);
-        cout <<"passed 1\n"<<endl; 
         /////////////////
         erode(binary, fg, Mat(), Point(-1,-1),3);
-        cout <<"passed 2\n"<<endl; 
 
         //////////////////
         dilate(binary,bg, Mat(), Point(-1,-1),3);
         threshold(bg,bg,1,128, THRESH_BINARY_INV);
-        cout <<"passed 3\n"<<endl; 
 
         ////////////////
         Mat markers(binary.size(), CV_8U, Scalar(0));
         markers = fg + bg;
-        cout <<"passed 4\n"<<endl; 
         ///////////////
         Segmenter seg;
         seg.setMarkers(markers);
-        cout <<"passed 5\n"<<endl; 
-        imshow("markers",markers);
-        waitKey(6000);
 
         result = seg.process(image);
         result.convertTo(result,CV_8U);
