7e71bda86ad45d28b5d33618681d3a93edcdfc3d
Marco Flowers <marco.flowers12@gmail.com>
2015-03-05 19:12:31 -0800

    MultiLookAggregator first commit

--- Verification/PythonVerif/MultiLookAggregator/Python/main.py
+++ Verification/PythonVerif/MultiLookAggregator/Python/main.py
@@ -1,48 +1,253 @@
-import numpy as np
+from math import radians, cos, sin, asin, sqrt
+import numpy as np 
 import cv2
+from PIL import Image
+import time
+import copy
+from operator import itemgetter
+import pickle
+import os
+from helper import *
 import pydbscancpplib
+from collections import Counter
 
-def doVerif(ShapeColorVals, ShapeColorStr, ShapeName, CharColorVals, CharColorStr, CharName, TargetLat, TargetLong, TargetOrientation, optionalArgs):
-	
-	print "MultilookAggregator (this is the Python)"
-	
-	pairwiseDistancesMat = np.zeros((6,6), np.float32)
-	
-	pairwiseDistancesMat[1,0] = 0.8
-	
-	pairwiseDistancesMat[2,0] = 0.03
-	pairwiseDistancesMat[2,1] = 0.9
-	
-	pairwiseDistancesMat[3,0] = 0.05
-	pairwiseDistancesMat[3,1] = 0.7
-	pairwiseDistancesMat[3,2] = 0.04
-	
-	pairwiseDistancesMat[4,0] = 0.85
-	pairwiseDistancesMat[4,1] = 0.1
-	pairwiseDistancesMat[4,2] = 0.68
-	pairwiseDistancesMat[4,3] = 0.74
-	
-	pairwiseDistancesMat[5,0] = 0.9
-	pairwiseDistancesMat[5,1] = 0.8
-	pairwiseDistancesMat[5,2] = 0.9
-	pairwiseDistancesMat[5,3] = 0.8
-	pairwiseDistancesMat[5,4] = 0.9
-	
-	# result should be: ((0,2,3),(1,4))
-	
-	print "================================================================================="
-	returnedClusters = pydbscancpplib.TargetClusterDBSCAN(pairwiseDistancesMat, 1, 0.3)
-	print "returnedClusters: ("+str(len(returnedClusters))+" clusters)"
-	for cluster in returnedClusters:
-		print "--- "+str(cluster)
-	print "================================================================================="
-	
-	if len(ShapeColorVals) != 3:
-		print "WARNING: ShapeColorVals wasn't a 3-element list!!!"
-	if len(CharColorVals) != 3:
-		print "WARNING: CharColorVals wasn't a 3-element list!!!"
-	
-	print "shape: "+ShapeName+", with color ("+str(ShapeColorVals[0])+","+str(ShapeColorVals[1])+","+str(ShapeColorVals[2])+") i.e. "+ShapeColorStr
-	print "char: "+CharName+", with color ("+str(CharColorVals[0])+","+str(CharColorVals[1])+","+str(CharColorVals[2])+") i.e. "+CharColorStr
-	print "target (lat, long) == ("+str(TargetLat)+", "+str(TargetLong)+"), orientation: "+str(TargetOrientation)
-	
+
+
+def doVerif(ShapeColorVals, ShapeColorStr, ShapeName, ShapeNameConfidence, CharColorVals, CharColorStr, CharNames, CharNameConfidences, TargetLat, TargetLong, TargetOrientation, askFJorClusrters):
+    
+    # create the new target
+    new_target = Target(scolor = ShapeColorStr,shape=ShapeName,shape_conf=ShapeNameConfidence, ccolor = CharColorStr, chars = CharNames,char_confs=CharNameConfidences, lat = TargetLat, long = TargetLong)
+    
+    # pass the target to the results
+    add_target(new_target)
+
+def sendJudges():
+    #format {"target":combinedTarget,"conf":confidenceLevel}
+    ranked_targets = get_ranked_results()
+    #get top 6
+    #save as judges.txt
+    with open("judges.txt","w") as f:
+        f.write('{:>12}  {:>12}  {:>12} {:>12} {:>12} {:>12}\n'.format("ShapeName", "ShapeColor", "Character", "CharColor","Lat","Long"))
+        for x in range(0,6):
+            if(len(ranked_targets)>x):
+                f.write(ranked_targets[x].judges_output())
+
+
+
+def getTopLoc():
+    ranked_targets = get_ranked_results()
+
+    # send top 50 lat long to other module
+
+
+class Target():
+    """
+    Defines a target with shape, shape color, char, char color, lat ,and long
+    """
+    def __init__(self,scolor=None,shape=None,shape_conf=0,ccolor=None,chars=None,char_confs=None,lat=0,long=0,overall_conf=0):
+        self.shape=shape
+        self.shape_conf = shape_conf
+        self.scolor=scolor
+        self.ccolor=ccolor
+        self.chars=chars
+        self.char_confs = char_confs
+        self.lat=lat
+        self.long=long
+        self.overall_conf=overall_conf
+    def check_distance(self,target):
+        """
+        Calculate distance between two targets
+        """
+        dist = haversine(target.long,target.lat,self.long,self.lat)
+        if(dist==0):
+            dist = .001
+        return (dist*dist)
+    def check_elements(self,target):
+        """
+        Calculates similarity between two targets attributes
+        """
+        total=0
+        if(self.shape == target.shape and self.shape != ""):
+            total+=(self.shape_conf*target.shape_conf)*2
+        if(self.scolor == target.scolor and self.scolor != ""):
+            total+=1
+        if(self.scolor == target.scolor and self.scolor != ""):
+            total+=1
+        if(self.ccolor == target.ccolor and self.ccolor != ""):
+            total+=1
+        return (1.0 - total/4.0)*(1.0 - total/4.0)
+    def __str__(self):
+        """
+        Prints out the target
+        """
+        char = ""
+        if(len(self.chars)>0):
+            char = self.chars[0]
+        return '{:>12}  {:>12}  {:>12} {:>12} '.format(xstr(self.shape), xstr(self.scolor), xstr(char), xstr(self.ccolor))
+    def judges_output(self):
+        """
+        Prints judges output
+        """
+        return '{:>12}  {:>12}  {:>12} {:>12} {:>12} {:>12}\n'.format(xstr(self.shape), xstr(self.scolor), xstr(self.chars), xstr(self.ccolor),xstr(self.lat),xstr(self.long))
+
+def guess_target(targets):
+    """
+    Return combined target from a cluster of targets
+    with a confidence
+    """
+    # get list of each attribute
+    shapes=[]
+    sconf=[]
+    scolors=[]
+    chars=[]
+    cconf=[]
+    ccolors=[]
+    total = 0
+    for target in targets:
+        total = total+1
+        shapes.append(target.shape)
+        sconf.append(target.shape_conf)
+        scolors.append(target.scolor)
+        chars.append(target.chars)
+        cconf.append(target.char_confs)
+        ccolors.append(target.ccolor)
+
+    # get the chars in a dict
+    chars_confs = bestChar(chars,cconf)
+    shapes_conf = aggregateList(shapes)
+    scolors_conf= aggregateList(scolors)
+    ccolors_conf =  aggregateList(ccolors)
+
+    top_char = highestItem(chars_confs)
+    top_shape = highestItem(shapes_conf)
+    top_sc = highestItem(scolors_conf)
+    top_cc = highestItem(ccolors_conf)
+
+    
+    #calc overall confidence
+    overall_conf = (top_char["conf"]*top_shape["conf"]*top_sc["conf"]*top_cc["conf"])/total
+    #create the combine Target
+    combined = Target(scolor=top_sc["name"],shape =top_shape["name"], shape_conf =1 , ccolor=top_cc["name"],
+        chars=top_char["name"],char_confs =1, lat=targets[0].lat,long=targets[0].long, overall_conf=overall_conf)
+
+    #return the target and confidence
+    return combined
+
+def aggregateList(lst):
+    """
+    Return most common item in a list with its occurance amount
+    """
+    common = {}
+    total = 0
+    #place items in the dict with how often they are seen
+    for item in lst:
+        total = total+1
+        if item in common:
+            common[item] = common[item]+1;
+        else:
+            common[item] = 1;
+
+    return common
+
+def highestItem(common):
+    #get greatest item in dict
+    highest = 0
+    name= ""
+    for result in common:
+        if(common[result]>=highest):
+            if name == "":
+                highest = common[result]
+                name = result
+    if name== "":
+        highest = 0
+    return {"name":name,"conf":highest}
+
+def bestChar(chars, char_confs):
+    # list of list of chars
+    # list of char confs
+    char_count = {}
+    count = 0
+    for listchars in chars:
+        index=0
+        for char in listchars:
+            if char in char_count:
+                char_count[char]+=char_confs[count][index]
+            else:
+                char_count[char] = char_confs[count][index]
+            index= index+1
+        count = count+1
+
+    return char_count
+
+
+def get_ranked_results():
+    """
+    Gets clusters of targets, ranks them and merges the targets into one
+    returns the list
+    """
+    try:
+        targets = pickle.load(open("targets.p","rb"))
+    except:
+        targets=[]
+
+    # create dist matrix
+    matrix = np.zeros((len(targets),len(targets)), np.float32)
+    sim_matrix = matrix_sim(matrix,targets)
+
+    if len(sim_matrix) > 3 :
+        #cluster targets
+        returnedClusters = pydbscancpplib.TargetClusterDBSCAN(sim_matrix, 1, 500)
+        outputTargets=[]
+        for cluster in returnedClusters:
+            clusterTargets = []
+            for item in cluster:
+                clusterTargets.append(targets[item])
+
+            #find what we think the target is
+            outputTargets.append(guess_target(clusterTargets))
+
+        #rank targets by conf
+        rankedTargets = sorted(outputTargets, key=lambda target:target.overall_conf,reverse=True)
+        print "Final Targets List with Confidence"
+        count = 0
+        for item in rankedTargets:
+            print str(item) + " conf="+ str(item.overall_conf)
+            count = count +1
+
+
+
+    return rankedTargets
+
+def add_target(new_target):
+    """
+    Takes in new target saves targets with pickle
+    """
+    # get old targets
+    try:
+        targets = pickle.load(open("targets.p","rb"))
+    except:
+        targets=[]
+    #append new target
+    targets.append(new_target)
+    
+    #save targets as pickle
+    pickle.dump(targets,open('targets.p',"wb"))
+
+
+#Main method call
+doVerif(0, "Teal"  , "Triangle"     ,1.0, 0, "Teal"  ,["I"],[1.0], 38.145014, -76.424933, 0, 0)
+doVerif(0, "Teal"  , "Triangle"     ,1.0, 0, "Teal"  ,[""],[1.0], 38.144964, -76.424901, 0, 0)
+doVerif(0, "Yellow", "Rectangle"    ,1.0, 0, "White" ,["N"],[1.0], 38.146039, -76.425706, 0, 0)
+doVerif(0, "Yellow", "Rectangle"    ,1.0, 0, "White" ,[""] ,[1.0], 38.146057, -76.42567, 0, 0)
+doVerif(0, "Black" ,""              ,1.0, 0, "Black" ,[""] ,[1.0], 38.146044, -76.425141, 0, 0)
+doVerif(0, "Orange", "QuarterCircle",1.0, 0, "Teal"  ,["B"],[1.0], 38.145117, -76.425853, 0, 0)
+doVerif(0, "Teal"  , "Triangle"     ,1.0, 0, "Teal"  ,[""] ,[1.0], 38.144993, -76.42495, 0, 0)
+doVerif(0, "Black" ,""              ,1.0, 0, "Teal"  ,[""] ,[1.0], 38.14666, -76.42628, 0, 0)
+doVerif(0, "Black" , "Semicircle"   ,1.0, 0, "Black" ,["O"],[1.0], 38.146456, -76.426586, 0, 0)
+doVerif(0, "Teal"  ,""              ,1.0, 0, "Yellow",[""] ,[1.0], 38.144477, -76.427055, 0, 0)
+doVerif(0, "Olive" ,""              ,1.0, 0, "Black" ,[""] ,[1.0], 38.144797, -76.426697, 0, 0)
+doVerif(0, "Teal"  ,""              ,1.0, 0, "Yellow",[""] ,[1.0], 38.144493, -76.427016, 0, 0)
+
+sendJudges()
+
+os.remove("targets.p")
