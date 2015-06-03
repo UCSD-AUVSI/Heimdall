187ccff608c40fbc0b92044e74a10adcb198d0d1
Monalp408@gmail.com <Monalp408@gmail.com>
2015-06-03 15:21:16 -0700

    added images to the segmentation folder

--- Recognition/Segmentation/PythonSegmentation/MonalSegmentation/Python/test.py
+++ Recognition/Segmentation/PythonSegmentation/MonalSegmentation/Python/test.py
@@ -4,6 +4,42 @@ import cv2
 import main
 
 
+
+
+
+
+#def testimg(sysargv):
+
+for tc in range(0,100):
+#for tc in tess:
+#for tc in prob:
+	#k=str(tc)+"_Junk.jpg"
+	k=str(tc)+"_0__crop.jpg"
+	#k=str(tc)+"_0__crop.png"
+	print "\n"+k
+
+	
+	loaded_image_mat = cv2.imread(k)
+
+	
+	#====================================================================
+	#	segmentation
+	(shapeSeg, shapeColor, charSeg, charColor) = main.doSegmentation(loaded_image_mat, 0,tc)
+	
+	#====================================================================
+	#	display image and results
+	
+#"""
+	cv2.imshow("original image", loaded_image_mat)
+	cv2.imshow("shape seg", shapeSeg*255) #images are floating point from 0.0 to 255.0, convert from 0.0 to 1.0 for imshow
+	cv2.imshow("char seg", charSeg*255)
+	cv2.waitKey(0) #wait for keypress
+
+#"""
+"""
+
+
+
 def testimg(sysargv):
 	#====================================================================
 	#	filename of image to load is a command line argument
@@ -47,3 +83,4 @@ def testimg(sysargv):
 # execute main()... this needs to be at the end
 if __name__ == "__main__":
 	testimg(sys.argv)
+#"""
